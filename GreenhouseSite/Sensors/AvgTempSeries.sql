9977275a5e6259e0f690edfdda097a3be28eb841
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-21 14:58:06 -0600

    Main Page graphs are now by hour

--- GreenhouseSite/Sensors/AvgTempSeries.sql
+++ GreenhouseSite/Sensors/AvgTempSeries.sql
@@ -1,6 +1,6 @@
 SELECT reading_datetime, round(AVG(value),2)
 FROM Sensors_Reading
 WHERE reading_type_id=%s
-GROUP BY reading_datetime
+GROUP BY strftime('%Y%m%d%H', reading_datetime)
 ORDER BY reading_datetime desc
 LIMIT 10
