59551276cbf46b11bf852f4fffad3c3b2407038a
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-01-23 19:16:05 -0600

    Added list_display and filters to Plants admin

--- GreenhouseSite/Plants/admin.py
+++ GreenhouseSite/Plants/admin.py
@@ -5,3 +5,19 @@ from . import models
 admin.site.register(models.Plant)
 admin.site.register(models.PlantType)
 admin.site.register(models.PlantStatus)
+
+
+@admin.register(models.PlantStatus)
+class PlantStatusAdmin(admin.ModelAdmin):
+    list_display = ["status_name"]
+
+
+@admin.register(models.PlantType)
+class PlantTypeAdmin(admin.ModelAdmin):
+    list_display = ["type_name", "determinate"]
+
+
+@admin.register(models.Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_filter = ["plant_status"]
+    list_display = ["plant_type", "plant_status", "date_sprouted"]
