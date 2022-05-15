ffbfd565ffa58b3b93419c5183f7a38493902f57
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-05-15 02:35:46 -0500

    Converted strftime to date_format

--- GreenhouseSite/Sensors/sql/AvgTankLevel.sql
+++ GreenhouseSite/Sensors/sql/AvgTankLevel.sql
@@ -2,7 +2,7 @@ SELECT (reading_datetime - sensor_dist) / height, round(AVG(value),2)
 FROM Sensors_reading
 JOIN Sensors_tank ON Sensors_tank.level_sensor_id = Sensors_reading.id
 WHERE Sensors_tank.id = %s
-GROUP BY strftime(%s, reading_datetime)
+GROUP BY DATE_FORMAT(reading_datetime, %s)
 ORDER BY reading_datetime desc
 LIMIT 10
 
