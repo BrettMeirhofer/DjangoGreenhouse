dba4437a39558bbed1d4b6566ec673633f4bb363
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-05-15 02:34:52 -0500

    Fixed tank sql errors

--- GreenhouseSite/Sensors/sql/AvgTankLevel.sql
+++ GreenhouseSite/Sensors/sql/AvgTankLevel.sql
@@ -1,6 +1,6 @@
-SELECT (reading_datetime - Sensors_tank.sensor_dist) / Sensors_tank.height, round(AVG(value),2)
+SELECT (reading_datetime - sensor_dist) / height, round(AVG(value),2)
 FROM Sensors_reading
-JOIN Sensors_tank ON Sensors_tank.level_sensor_id = reading_datetime.id
+JOIN Sensors_tank ON Sensors_tank.level_sensor_id = Sensors_reading.id
 WHERE Sensors_tank.id = %s
 GROUP BY strftime(%s, reading_datetime)
 ORDER BY reading_datetime desc
