2f9fe9a23a70b9ff8aecff6cdb305929d71ec749
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-05-15 15:36:07 -0500

    Update AvgTankLevel.sql

--- GreenhouseSite/Sensors/sql/AvgTankLevel.sql
+++ GreenhouseSite/Sensors/sql/AvgTankLevel.sql
@@ -1,10 +1,10 @@
-SELECT reading_datetime, round((AVG(value) - sensor_dist) / height, 2)
+SELECT reading_datetime, ((AVG(value) - sensor_dist) / height) * 100
 FROM Sensors_reading
 JOIN Sensors_tank ON Sensors_tank.level_sensor_id = Sensors_reading.sensor_id
 WHERE Sensors_tank.id = %s
 GROUP BY DATE_FORMAT(reading_datetime, %s)
 ORDER BY reading_datetime desc
-LIMIT 10
+LIMIT 10;
 
 
 
