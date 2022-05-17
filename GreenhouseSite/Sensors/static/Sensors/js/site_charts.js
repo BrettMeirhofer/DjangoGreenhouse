82ac326aadb10aa86cf7dcae2eaa3d57a3fadbb9
Brett-Meirhofer <brettmeirhofer@gmail.com>
2022-05-16 21:40:08 -0500

    Update site_charts.js

--- GreenhouseSite/Sensors/static/Sensors/js/site_charts.js
+++ GreenhouseSite/Sensors/static/Sensors/js/site_charts.js
@@ -25,7 +25,7 @@ function get_hours_string(deltas){
     times = []
     deltas.forEach((element => {
         current_time = new Date(Date.parse(element) * 1000)
-        console.log(current_time)
+        console.log(element + " " + current_time)
         current_time = current_time.getHours() + ":00"
         times.push(current_time)
     }));
